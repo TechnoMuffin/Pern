b4a07577cb940ceca0f6e3d552fc817773cf608d
Adriex <adriel.chagay@gmail.com>
2016-06-16 09:48:04 -0300

    Hotfixes varios

--- Django/webTaller/webTaller/urls.py
+++ Django/webTaller/webTaller/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include("testapp.urls", namespace = "app_testapp")),
-    url(r'register_user/^', include("teacherRegister.urls", namespace = "app_teacherRegister")),
+    url(r'^register_user/', include("teacherRegister.urls", namespace = "app_teacherRegister")),
 ]
