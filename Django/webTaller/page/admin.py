6fd54422ca1cac61193d9a6086e64d068aaf200f
franheredia <franheredia.ov@gmail.com>
2016-06-16 11:19:08 -0300

    Terminadas las entidades del models.py

--- Django/webTaller/page/admin.py
+++ Django/webTaller/page/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Pupil, Course, Subject, PupilFollowing
+from .models import Pupil, Course, Subject, PupilFollowing, Fulfillment, Projects, ProjectStages
 
 admin.site.register(Pupil)
 admin.site.register(Course)
 admin.site.register(Subject)
 admin.site.register(PupilFollowing)
+admin.site.register(Fulfillment)
+admin.site.register(Projects)
+admin.site.register(ProjectStages)
--- Django/webTaller/page/models.py
+++ Django/webTaller/page/models.py
@@ -8,25 +8,25 @@ from django.db import models
 class Course(models.Model):
     idCourse = models.AutoField(primary_key=True)
     yearCourse = models.IntegerField()
-    DIVISION_OPTIONS = (
-    (u'A', u'Division A'),
-    (u'B', u'Division B'),
-    (u'C', u'Division C'),
+    COURSE_OPTIONS = (
+    (u'1A', u'1º año "A"'),
+    (u'1B', u'1º año "B"'),
+    (u'1C', u'1º año "C"'),
+    (u'2A', u'2º año "A"'),
+    (u'2B', u'2º año "B"'),
+    (u'2C', u'2º año "C"'),
+    (u'3A', u'3º año "A"'),
+    (u'3B', u'3º año "B"'),
+    (u'3C', u'3º año "C"'),
     )
-    YEAR_OPTIONS = (
-    (u'1', u'1er Año'),
-    (u'2', u'2do Año'),
-    (u'3', u'3er Año'),
-    )
-    divisionCourse = models.CharField(max_length=128, choices=DIVISION_OPTIONS)
-    divisionYear = models.CharField(max_length=128, choices=YEAR_OPTIONS)
+    courseType = models.CharField(max_length=128, choices=COURSE_OPTIONS)
 
     class Meta:
         verbose_name = 'Course'
         verbose_name_plural = 'Courses'
         
     def __str__(self):
-        tagName = str(self.divisionYear) + " " + str(self.divisionCourse) + "(" + str(self.yearCourse) + ")"
+        tagName = str(self.courseType) + " - " + str(self.yearCourse)
         return tagName
     
 ##################################ALUMNO##################################
@@ -36,20 +36,31 @@ class Pupil(models.Model):
     surnamePupil = models.CharField(max_length=128)
     idPupil = models.AutoField(primary_key=True)
     idCourse = models.ForeignKey(Course)
-    
-    
+
     class Meta:
         verbose_name = 'Pupil'
         verbose_name_plural = 'Pupils'
     def __str__(self):
-        tagName = str(self.namePupil) +" " + str(self.surnamePupil)
+        tagName = str(self.namePupil) +" " + str(self.surnamePupil) + " [" + str(self.idCourse) + "]"
         return tagName
 
+##################################CUMPLIMIENTOS##################################
+class Fulfillment(models.Model):
+    nameFF = models.CharField(max_length=128, primary_key=True)
+    
+    class Meta:
+        verbose_name = 'Fulfillment'
+        verbose_name_plural = 'Fulfillments'
+    def __str__(self):
+        tagName = str(self.nameFF)
+        return tagName
+    
 ##################################MATERIA##################################
 
 class Subject(models.Model):
     idSubject = models.AutoField(primary_key=True)
     nameSubject = models.CharField(max_length=128)
+    idFF = models.ManyToManyField(Fulfillment, blank=True)
     idCourse = models.ManyToManyField(Course)
     
     class Meta:
@@ -66,13 +77,37 @@ class PupilFollowing(models.Model):
     datePF = models.DateField()
     idSubject = models.ForeignKey(Subject)
     idPupil = models.ForeignKey(Pupil)
-    commentPF = models.TextField()
+    commentPF = models.TextField(blank=True)
     
     class Meta:
         verbose_name = 'Pupil Following'
         verbose_name_plural = 'Pupils Following'
     def __str__(self):
-        print str(self.idSubject)
-        tagName = str(Pupil(self.idPupil)) + "(" + str(self.idSubject) + ")"
+        tagName = str(self.idPupil) + "(" + str(self.idSubject) + ")"
+        return tagName
+    
+##################################PROYECTOS##################################
+class Projects(models.Model):
+    nameProject = models.CharField(max_length=128, primary_key=True)
+    idSubject = models.ForeignKey(Subject)
+    
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+    def __str__(self):
+        tagName = str(self.nameProject) + "[" + str(self.idSubject) + "]"
+        return tagName
+    
+##################################ETAPAS DE PROYECTOS##################################
+class ProjectStages(models.Model):
+    namePS = models.CharField(max_length=128)
+    idPS = models.AutoField(primary_key=True)
+    idProject = models.ForeignKey(Projects)
+    
+    class Meta:
+        verbose_name = 'Project Stage'
+        verbose_name_plural = 'Project Stages'
+    def __str__(self):
+        tagName = str(self.namePS) + "[" + str(self.idProject) +"]"
         return tagName
     
