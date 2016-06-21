72fb3bee59796980a48d865f1753db072647102d
franheredia <franheredia.ov@gmail.com>
2016-06-21 13:15:51 -0300

    Agregada funcionalidad para cmbCourse

--- Django/webTaller/testapp/views.py
+++ Django/webTaller/testapp/views.py
@@ -1,27 +1,35 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response, render, redirect
+from page.models import Course
 
 # Create your views here.
 
 def base(request):
         context = RequestContext(request)
         return render_to_response('base.html',context)
+    
 def seguimientoAl(request):
     context = RequestContext(request)
-    return render_to_response('SeguimientoAlumno.html',context)
+    courses = Course.objects.all() 
+    return render_to_response('SeguimientoAlumno.html', {'courses':courses},context)
+
 def registerUser(request):
         context = RequestContext(request)
         return render_to_response('CreaUsuario.html',context)
+    
 def loginUser(request):
         context = RequestContext(request)
         return render_to_response('Login.html',context)
+    
 def olvidaContra(request):
         context = RequestContext(request)
         return render_to_response('OlvidoContra.html',context)
+    
 def loginAdmin(request):
         context = RequestContext(request)
         return render_to_response('LogAdmin.html',context)
+    
 def documentos(request):
         context = RequestContext(request)
         return render_to_response('Documentos.html',context)
