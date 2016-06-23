b7112ba747c6482222d3f3758e1c62216fdbc60b
franheredia <franheredia.ov@gmail.com>
2016-06-23 20:17:46 -0300

    Filtrado de Materias y Alumno por Cursos terminado

--- Django/webTaller/page/models.py
+++ Django/webTaller/page/models.py
@@ -2,31 +2,57 @@
 from __future__ import unicode_literals
 from django.db import models
     
+
+##################################CUMPLIMIENTOS##################################
+class Fulfillment(models.Model):
+    nameFF = models.CharField(max_length=128, primary_key=True)
     
+    class Meta:
+        verbose_name = 'Fulfillment'
+        verbose_name_plural = 'Fulfillments'
+    def __str__(self):
+        tagName = str(self.nameFF)
+        return tagName
+
 ##################################CURSOS##################################
 
 class Course(models.Model):
     idCourse = models.AutoField(primary_key=True)
     yearCourse = models.IntegerField()
     COURSE_OPTIONS = (
-    (u'1A', u'1º año "A"'),
-    (u'1B', u'1º año "B"'),
-    (u'1C', u'1º año "C"'),
-    (u'2A', u'2º año "A"'),
-    (u'2B', u'2º año "B"'),
-    (u'2C', u'2º año "C"'),
-    (u'3A', u'3º año "A"'),
-    (u'3B', u'3º año "B"'),
-    (u'3C', u'3º año "C"'),
+    (u'1', u'1º año'),
+    (u'2', u'2º año'),
+    (u'3', u'3º año'),
+    )
+    DIVISION_OPTIONS = (
+    (u'A', u'A'),
+    (u'B', u'B'),
+    (u'C', u'C'),
     )
     courseType = models.CharField(max_length=128, choices=COURSE_OPTIONS)
+    courseDivision = models.CharField(max_length=128, choices=DIVISION_OPTIONS)
 
     class Meta:
         verbose_name = 'Course'
         verbose_name_plural = 'Courses'
         
     def __str__(self):
-        tagName = str(self.courseType) + " - " + str(self.yearCourse)
+        tagName = str(self.courseType) + str(self.courseDivision) + " - " + str(self.yearCourse)
+        return tagName
+    
+##################################MATERIA##################################
+
+class Subject(models.Model):
+    idSubject = models.AutoField(primary_key=True)
+    nameSubject = models.CharField(max_length=128)
+    idFF = models.ManyToManyField(Fulfillment, blank=True)
+    idCourse = models.ManyToManyField(Course, blank=True)
+    
+    class Meta:
+        verbose_name = 'Subject'
+        verbose_name_plural = 'Subjects'
+    def __str__(self):
+        tagName = str(self.nameSubject)
         return tagName
     
 ##################################ALUMNO##################################
@@ -44,32 +70,6 @@ class Pupil(models.Model):
         tagName = str(self.namePupil) +" " + str(self.surnamePupil) + " [" + str(self.idCourse) + "]"
         return tagName
 
-##################################CUMPLIMIENTOS##################################
-class Fulfillment(models.Model):
-    nameFF = models.CharField(max_length=128, primary_key=True)
-    
-    class Meta:
-        verbose_name = 'Fulfillment'
-        verbose_name_plural = 'Fulfillments'
-    def __str__(self):
-        tagName = str(self.nameFF)
-        return tagName
-    
-##################################MATERIA##################################
-
-class Subject(models.Model):
-    idSubject = models.AutoField(primary_key=True)
-    nameSubject = models.CharField(max_length=128)
-    idFF = models.ManyToManyField(Fulfillment, blank=True)
-    idCourse = models.ManyToManyField(Course)
-    
-    class Meta:
-        verbose_name = 'Subject'
-        verbose_name_plural = 'Subjects'
-    def __str__(self):
-        tagName = str(self.nameSubject)
-        return tagName
-    
 ##################################SEGUIMIENTO DE ALUMNO##################################
 class PupilFollowing(models.Model):
     idPF = models.AutoField(primary_key=True)
--- Django/webTaller/testapp/views.py
+++ Django/webTaller/testapp/views.py
@@ -2,19 +2,36 @@ from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response, render, redirect
 from page.models import Course, Pupil, Subject
-
-# Create your views here.
+from django.core import serializers
+from django.http import HttpResponse
+import json
 
 def base(request):
         context = RequestContext(request)
         return render_to_response('base.html',context)
     
 def seguimientoAl(request):
-    context = RequestContext(request)
-    courses = Course.objects.all() 
-    pupils = Pupil.objects.all() 
-    subjects = Subject.objects.all() 
-    return render_to_response('SeguimientoAlumno.html', {'courses':courses, 'subjects':subjects, 'pupils':pupils},context)
+        if request.is_ajax():
+            queryid = request.GET.get('queryId')
+            if(queryid == "course"):
+                idC = request.GET.get('idCourse')
+                courses = Course.objects.filter(idCourse=int(idC)) 
+                info = serializers.serialize('json', courses)
+            elif(queryid == "subject"):
+                idC = request.GET.get('idCourse')
+                subject = Subject.objects.filter(idCourse=int(idC))
+                info = serializers.serialize('json', subject)
+            elif(queryid == "pupil"):
+                idC = request.GET.get('idCourse')
+                pupil = Pupil.objects.filter(idCourse=int(idC))
+                info = serializers.serialize('json', pupil)
+            return HttpResponse(info)
+        else:
+            context = RequestContext(request)
+            courses = Course.objects.all() 
+            pupils = Pupil.objects.all() 
+            subjects = Subject.objects.all() 
+        return render_to_response('SeguimientoAlumno.html', {'courses':courses, 'subjects':subjects, 'pupils':pupils},context)
 
 def registerUser(request):
         context = RequestContext(request)
