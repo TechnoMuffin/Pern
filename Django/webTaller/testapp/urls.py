f90cf5f41139d3d9c4d74835681843d76a3c234f
Adriex <adriel.chagay@gmail.com>
2016-06-27 11:32:34 -0300

    Hotfix

--- Django/webTaller/testapp/urls.py
+++ Django/webTaller/testapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from testapp import views
 
 urlpatterns = [
-    url(r'^', views.loginUser),
+    url(r'^$', views.loginUser),
     url(r'^base$', views.base),
     url(r'^seguimientoAlumno$', views.seguimientoAl, name='seg-al'),
     url(r'^olvidacontra$', views.olvidaContra),
