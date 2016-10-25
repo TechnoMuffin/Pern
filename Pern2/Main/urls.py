13f6e4195d29a753af1fe384aae0e20fd82bbf9d
Adriex <adrielchagay@gmail.com>
2016-10-25 10:37:38 -0300

    RECOSO

--- Pern2/Main/urls.py
+++ Pern2/Main/urls.py
@@ -4,5 +4,5 @@ import views
 urlpatterns = [
     url(r'^seguimientoAlumno$', views.pupilFollowing, name='seg-al'),
     url(r'^historial$', views.history, name='history'),
-    url(r'^rotacionAlumno$', views.pupilFollowing, name='rot-al'),
+    url(r'^rotacionAlumno$', views.rotation, name='rot-al'),
 ]
