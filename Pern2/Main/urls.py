cebf0d2c31cd89053fd30810b619900bf4e2b1be
franheredia <franheredia.ov@gmail.com>
2016-11-28 10:45:12 -0300

    Completadas funcionalidades de seguimiento-alumno en Pern2

--- Pern2/Main/urls.py
+++ Pern2/Main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^historial$', views.history, name='history'),
     url(r'^rotacionAlumno$', views.rotation, name='rot-al'),
     url(r'^proyectos$', views.projectFollowing, name='projects'),
+    url(r'^makeQueriesStudentFollowing$', views.StudentFollowingAjaxQueries, name='sfAjax'),
     url(r'^documents$', views.document, name='doc'),
     url(r'^modulos$', views.modules, name='modules')
 ]
--- Pern2/Main/views.py
+++ Pern2/Main/views.py
@@ -41,7 +41,7 @@ def pupilFollowing(request):
             idStudent = request.GET.get('idStudent')
             date = request.GET.get('date')
             if(idModule != '' or idProject != '' or idStudent != '' or idStudent != '' or date != ''):
-                sf = StudentFollowing.objects.get(exists=True,idModule__idModule=int(idModule), idStudent__idUser=int(idStudent), dateSF=str(date))
+                sf = StudentFollowingModel.objects.get(exists=True,idModule__idModule=int(idModule), idStudent__idUser=int(idStudent), dateSF=str(date))
                 dailyFF = dailyFulfillment.objects.filter(idSF=sf)
                 if not dailyFF:
                     modelFF = Fulfillment.objects.filter(idProject__idProject=int(idProject))
@@ -97,7 +97,7 @@ def pupilFollowing(request):
             module = request.GET.get('idModule')
             module = Module.objects.get(idModule=int(module))
             students = Student.objects.filter(idRotation__idModule=module).order_by('surname')
-            sfs = StudentFollowing.objects.filter(
+            sfs = StudentFollowingModel.objects.filter(
                 dateSF=str(date), idModule=module)
             if not sfs:
                 for i in students:
@@ -239,7 +239,7 @@ def pupilFollowing(request):
             # Devuelve el alumno pedido
             idUser = request.GET.get('idStudent')
             student = Student.objects.filter(idUser=int(idUser))
-            studentFollowings = StudentFollowing.objects.filter(exists=True,idStudent=student).order_by('-dateSF')
+            studentFollowings = StudentFollowingModel.objects.filter(exists=True,idStudent=student).order_by('-dateSF')
             workOn = OnClass.objects.all()
             coso = list(chain(studentFollowings, workOn))
             info = serializers.serialize(
@@ -279,7 +279,7 @@ def pupilFollowing(request):
         courses = Course.objects.all()
         pupils = Student.objects.all()
         subjects = Module.objects.all()
-        return render_to_response('pupilFollowing.html', {'courses':courses, 'subjects':subjects, 'pupils':pupils},context)
+        return render_to_response('studentsFollowing.html', {'courses':courses, 'subjects':subjects, 'pupils':pupils},context)
 
 def history(request):
     context = RequestContext(request)
@@ -544,3 +544,189 @@ def projectFollowing(request):
         context = RequestContext(request)
         courses = Course.objects.all()
         return render_to_response('proyectos.html', {'courses':courses}, context)
+
+#PETICIONES PARA SEGUIMIENTO DE ALUMNOS
+def StudentFollowingAjaxQueries(request):
+    queryId = request.GET.get('queryId')
+    info = ''
+    data = ''
+
+    if(queryId == 'modulesByCourse'):
+        idC = int(request.GET.get('idCourse'))
+        data = Module.objects.filter(idCourse__idCourse=idC)
+
+    elif(queryId == 'studentsByRotation'):
+        idR = request.GET.get('idRotation')
+        data = Student.objects.filter(idRotation__idRotation=idR).order_by('surname')
+
+    elif(queryId == 'rotationsByModule'):
+        module = request.GET.get('idModule')
+        data = Rotation.objects.filter(idModule__idModule=module)
+
+    elif(queryId == 'projectsByModule'):
+        idM = int(request.GET.get('module'))
+        data = Project.objects.filter(idModule__idModule=idM)
+
+    elif(queryId == 'studentById'):
+        idU = int(request.GET.get('idStudent'))
+        data = Student.objects.filter(idUser=idU)
+
+    elif(queryId == 'activitiesByProject'):
+        idP = int(request.GET.get('idProject'))
+        data = Activity.objects.filter(idProject__idProject=idP)
+
+    elif(queryId == 'working'):
+        idS  = request.GET.get('idStudent')
+        idA = request.GET.get('idActivity')
+        student = Student.objects.get(idUser=idS)
+        activity = Activity.objects.get(idActivity=idA)
+        try:
+            Working.objects.get(idStudent__idUser=idS, idActivity__idActivity=idA)
+        except ObjectDoesNotExist:
+            newWorking = Working()
+            newWorking.idStudent = student
+            newWorking.idActivity = activity
+            newWorking.numberOfClasses = 0
+            newWorking.hasFinish = False
+            newWorking.save()
+        data = Working.objects.filter(idStudent__idUser=idS, idActivity__idActivity=idA)
+        numberOC = len(OnClass.objects.filter(idActivity__idActivity=idA,idSF__idStudent__idUser=idS))
+        data.update(numberOfClasses=numberOC)
+
+
+    elif(queryId == "activitiesByProject"):
+        idP = int(request.GET.get('idProject'))
+        data = Activity.objects.filter(idProject__idProject=idP)
+
+    elif(queryId == "getCurrentActivity"):
+        data = []
+        idM = request.GET.get('idModule')
+        date = request.GET.get('date')
+        studentsFollowing = StudentFollowingModel.objects.filter(dateSF=str(date),idModule__idModule=idM)
+        for x in studentsFollowing:
+            work = OnClass.objects.get(idSF=x).idActivity
+            if(work != None):
+                dictionary = {'studentId': x.idStudent.idUser,'activity': work.idActivity}
+            else:
+                dictionary = {'studentId': x.idStudent.idUser,'activity': "zero"}
+            data.append(dictionary)
+        data = json.dumps(data)
+        print("\033[1m RESPUESTA A " + queryId + "\033[0m")
+        print(data)
+        return HttpResponse(data)
+
+    elif(queryId == "saveActivity"):
+        idS = request.GET.get('idStudent')
+        idM = request.GET.get('idModule')
+        idA = request.GET.get('activity')
+        date = request.GET.get('date')
+        data = OnClass.objects.filter(idSF__idStudent__idUser=idS, idSF__idModule__idModule=idM, idSF__dateSF=date)
+        if(idA != "zero"):
+            activity = Activity.objects.get(idActivity=idA)
+            data.update(idActivity=activity)
+        else:
+            data.update(idActivity=None)
+
+    elif(queryId == "createAllSF"):
+        date = request.GET.get('date')
+        module = request.GET.get('idModule')
+        students = Student.objects.filter(idRotation__idModule__idModule=module).order_by('surname')
+        for student in students:
+            try:
+                StudentFollowingModel.objects.get(dateSF=str(date),idModule__idModule=module, idStudent__idUser= student.pk)
+            except ObjectDoesNotExist:
+                newSF = StudentFollowingModel()
+                newSF.presenceSF = True
+                newSF.dateSF = str(date)
+                newSF.idModule = Module.objects.get(idModule=module)
+                newSF.idStudent = student
+                newSF.commentPF = ''
+                newSF.save()
+                newSF.idTeacher.add(Teacher.objects.get(idUser=1)) #TODO meter un profesor de verdad
+                newSF.save()
+        data = StudentFollowingModel.objects.filter(dateSF=str(date),idModule=module)
+
+    elif(queryId == "createAllCW"):
+        date = request.GET.get('date')
+        module = request.GET.get('idModule')
+        sFs = StudentFollowingModel.objects.filter(dateSF=str(date),idModule__idModule=module)
+        for sF in sFs:
+            try:
+                OnClass.objects.get(idSF=sF)
+            except ObjectDoesNotExist:
+                newOc = OnClass()
+                newOc.idSF = sF
+                newOc.save()
+        data = OnClass.objects.filter(idSF__idModule=module,idSF__dateSF=date)
+
+    elif(queryId == "createAllFF"):
+        idP = request.GET.get('idProject')
+        idS = request.GET.get('idStudent')
+        idM = request.GET.get('idModule')
+        date = request.GET.get('date')
+        fulfillments = Fulfillment.objects.filter(idProject__idProject=idP).order_by('nameFF')
+        studentFollowing = StudentFollowingModel.objects.get(dateSF=str(date),idModule__idModule=idM, idStudent__idUser= idS)
+        for fulfillment in fulfillments:
+            try:
+                classFulfillment.objects.get(idFF=fulfillment, idSF=studentFollowing)
+            except ObjectDoesNotExist:
+                data = classFulfillment()
+                data.idFF = fulfillment
+                data.idSF = studentFollowing
+                data.check = True
+                data.save()
+        data = classFulfillment.objects.filter(idFF__idProject=idP,idSF=studentFollowing)
+
+
+    elif(queryId == "getStudentFollowing"):
+        idS = request.GET.get('idStudent')
+        idM = request.GET.get('idModule')
+        date = request.GET.get('date')
+        data = StudentFollowingModel.objects.filter(dateSF=str(date),idModule__idModule=idM, idStudent__idUser= idS)
+
+    elif(queryId == 'saveFulfillment'):
+        idS = request.GET.get('idStudent')
+        idM = request.GET.get('idModule')
+        date = request.GET.get('date')
+        idFF = request.GET.get('fulfillmentId')
+        studentF  = StudentFollowingModel.objects.filter(dateSF=str(date),idModule__idModule=idM, idStudent__idUser= idS)
+        data = classFulfillment.objects.filter(idFF__idFF=idFF,idSF=studentF)
+        data.update(check = not data[0].check)
+
+    elif(queryId == 'calcAccuracy'):
+        idS = request.GET.get('idStudent')
+        idM = request.GET.get('idModule')
+        idFF = request.GET.get('idFF')
+        classQuantity  = len(classFulfillment.objects.filter(idFF__idFF=idFF,idSF__idStudent__idUser=idS))
+        classTrue  = len(classFulfillment.objects.filter(idFF__idFF=idFF,idSF__idStudent__idUser=idS,check=True))
+        info  = (float(classTrue) / float(classQuantity))*100
+        return HttpResponse(info)
+
+
+    elif(queryId == 'saveObservations'):
+        idS = request.GET.get('idStudent')
+        idM = request.GET.get('idModule')
+        date = request.GET.get('date')
+        text = request.GET.get('text')
+        data = StudentFollowingModel.objects.filter(dateSF=str(date),idModule__idModule=idM, idStudent__idUser= idS)
+        data.update(commentPF = text)
+
+    elif(queryId == 'savePresence'):
+        idS = request.GET.get('idStudent')
+        idM = request.GET.get('idModule')
+        date = request.GET.get('date')
+        data = StudentFollowingModel.objects.filter(dateSF=str(date),idModule__idModule=idM, idStudent__idUser= idS)
+        data.update(presenceSF= not data[0].presenceSF)
+
+    elif(queryId == 'fulfillmentsByProject'):
+        idP = request.GET.get('idProject')
+        data = Fulfillment.objects.filter(idProject__idProject=idP).order_by('nameFF')
+
+
+    if(data != ''):
+        info = serializers.serialize('json', data)
+    else:
+        info = "La siguiente queryId no existe: '" + queryId +"'"
+    print("\033[1m RESPUESTA A " + queryId + "\033[0m")
+    print(info)
+    return HttpResponse(info)
